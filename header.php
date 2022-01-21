dab820a923e1dbf5b0e95271fc4e16be44065fc5
NelsonJ <jesus.nels@gmail.com>
2022-01-21 15:40:08 +0100

    Initial commit - adding index page and some images

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='stylesheet' href='./style.css'>
+    <link rel='icon' href='favicon.ico'>
+    <title>My Resume</title>
+</head>
+<body>
+    <div class='main'>
+        <header>
+            <div>
+                <h1> Nelson JESUS </h1>
+                <h2> Junior Web Developer </h2>
+                <a href='contact.html'> Contact me </a>
+            </div>
+            <img id='profilePicture' src='./img/profil_beach.png' alt='my profil picture'>
+        </header>
