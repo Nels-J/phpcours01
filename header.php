9646a7bbc7eb10f8e045c6295b90a5833904a46e
NelsonJ <jesus.nels@gmail.com>
2022-01-24 11:24:43 +0100

    updated link icon to reflect change favicon.png to favicon.ico

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <link rel='stylesheet' href='./style.css'>
-    <link rel='icon' href='favicon.ico'>
+    <link rel='icon' href='./favicon.ico'>
     <title><?= $metaTitle?></title>
     <meta name="description" content="<?= $metaDescription?>">
 </head>
@@ -14,7 +14,7 @@
         <header>
                 <h1> Nelson JESUS </h1>
                 <h2> Junior Web Developer </h2>
-                <a href='contact.html'> Contact me </a>
+                <a href='contact.php'> Contact me </a>
             </div>
             <img id='profilePicture' src='./img/profil_beach.png' alt='my profil picture'>
         </header>
