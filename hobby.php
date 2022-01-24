850660a514ca08aef60a23a0894515877e7da433
NelsonJ <jesus.nels@gmail.com>
2022-01-24 11:29:48 +0100

    Refactor: hobby.html on hobby.php including header and footer as well

---
+++ hobby.php
@@ -0,0 +1,29 @@
+<?php
+$metaTitle = 'Hobbies';
+$metaDescription = 'An amazing page about my hobbies';
+include 'header.php';
+?>
+
+        <section>
+
+            <h3> Here you can look deeper on one of my favourite hobbies </h3>
+
+            <p> I have a passion for a very special kind of cars, they are known as low-riders cars or Seven-likes cars </p>
+        
+        </section>
+
+        <section class="hobbyVideo">
+
+            <h4> Comval Racing Portugal in Braga circuit </h4>
+            
+            <iframe src="https://www.youtube-nocookie.com/embed/C7FdcZN8eY0?start=26" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        
+        </section>
+        <section>
+            <h4>More links related on :</h4>
+            <a href="https://www.caterhamcars.com/en">Official Caterham Cars website</a>
+            <a href="https://westfield-sportscars.co.uk/">Official Westfield website</a>
+        </section>
+
+
+<?php include 'footer.php'; ?>
