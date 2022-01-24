5d99c4fe5e2550811fbbcc7c26fc84c4a9c0f688
NelsonJ <jesus.nels@gmail.com>
2022-01-24 15:31:37 +0100

    Feature: Creating routes

--- index.php
+++ index.php
@@ -3,10 +3,16 @@ $metaTitle = 'Accueil';
 $metaDescription = 'Ceci est une super description';
 include 'header.php';
 
-?>
+//include './pages/resume.php';
+if (isset($_GET['page'])) {
+    $page=$_GET['page'];
+    if ($page == 'hobby') {
+      include 'pages/hobby.php';
+    } elseif ($page == 'contact') {
+        include 'pages/contact.php';
+    } else {
+        include 'pages/resume.php';
+    }
+}
 
-<?php
-include './pages/resume.php';
-?>
-
-<?php include 'footer.php'; ?>
+include 'footer.php';
