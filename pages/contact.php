fcaf6cb8d8950b294f387d5dc5d3cddd9a6e3d41
NelsonJ <jesus.nels@gmail.com>
2022-01-25 14:45:48 +0100

    Refactor: step 4.1 form refactored to follow the rules and action form updated by ../index.php?page=contact

--- pages/contact.php
+++ pages/contact.php
@@ -5,33 +5,39 @@ $metaDescription = 'Ceci est un super top formulaire de contact';
 
             <h3> Please feel free to email me filling the form bellow: </h3>
 
-            <form method="post" action="/post">
-                
+            <form method="post" action="../index.php?page=contact">
+
                 <fieldset>
 
                     <legend> Your details: </legend>
                     <p>
-                        <label>Your name: <input name="custname"></label>
-                    </p>
-                    <p>
-                        <label>Telephone: <input type=tel name="custtel"></label>
-                    </p>
-                    <p>
-                        <label>Your Mail: <input type=email name="custemail"></label>
-                    </p>
-                
-                    <p>
                         <label for="gender-select">Gender:</label>
 
                         <select name="genders" id="gender-select">
-                        
+
                             <option value="">--Please choose an option--</option>
                             <option value="Man">Man</option>
                             <option value="Woman">Woman</option>
                             <option value="The 3rd sex">I'm not binary at all !</option>
-                        
+
                         </select>
-                            
+
+                    </p>
+
+                    <p>
+                        <label>Your Surname: <input name="surName"></label>
+                    </p>
+
+                    <p>
+                        <label>Your Firstname: <input name="firstName"></label>
+                    </p>
+
+                    <p>
+                        <label>Phone: <input type=tel name="phone"></label>
+                    </p>
+
+                    <p>
+                        <label>Your Mail: <input type=email name="email"></label>
                     </p>
 
                 </fieldset>
@@ -41,16 +47,16 @@ $metaDescription = 'Ceci est un super top formulaire de contact';
                     <legend> Subject: </legend>
                  
                         <p>
-                            <label> <input type="radio" name="topping" value="bacon"> Enquiry </label>
+                            <label> <input type="radio" name="subject" value="Enquiry"> Enquiry </label>
                         </p>
                         <p>
-                            <label> <input type="radio" name="topping" value="cheese"> Feedback </label>
+                            <label> <input type="radio" name="subject" value="Feedback"> Feedback </label>
                         </p>
                         <p>
-                            <label> <input type="radio" name="topping" value="onion"> Meet </label>
+                            <label> <input type="radio" name="subject" value="Meet"> Meet </label>
                         </p>
                         <p>
-                            <label> <input type="radio" name="topping" value="mushroom"> Other </label>
+                            <label> <input type="radio" name="subject" value="Other"> Other </label>
                         </p>
 
                 </fieldset>
